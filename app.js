fa8f72fa4176b97a90b9805ab6221a2aa1602f15
TahseenSust <tahseen19@student.sust.edu>
2022-03-02 21:34:10 +0600

    added invalid route handler

--- app.js
+++ app.js
@@ -12,16 +12,24 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  //   console.log('hello from middleware');
-  next();
+    //   console.log('hello from middleware');
+    next();
 });
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+    req.requestTime = new Date().toISOString();
+    next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// should be in last part of route handler
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Cant find ${req.originalUrl} on this server!`,
+    });
+});
 
 module.exports = app;
