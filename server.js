28c9a320cc9e2ab05fb25ab3deb2e02a58f8f504
TahseenSust <tahseen19@student.sust.edu>
2022-03-07 20:55:39 +0600

    uncaught exceptions

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN...');
+    console.log(err.name, err.message);
+    process.exit(1);
+})
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-
 
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
@@ -26,9 +31,9 @@ const server = app.listen(port, () => {
     console.log(`app running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
-    console.log(err.name, err.message);
+process.on('unhandledRejection', (err) => {
     console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+    console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
     });
